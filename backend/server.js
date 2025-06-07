96a5191f4d4c0a26aa65a1f5580d060895180c91
Tanushree Dhote <142327828+TanushreeDhote06@users.noreply.github.com>
2025-06-07 23:25:46 +0530

    updated with the postgresSQL connection

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,27 @@
-const express = require('express');
-const app = express();
-const port = 8000;
+// const express = require('express');
+// const app = express();
+// const port = 8000;
 
-app.get('/test', (req, res) => {
-res.send('Test Successful.');
+// app.get('/test', (req, res) => {
+// res.send('Test Successful.');
+// });
+
+// app.listen(port, () => {
+// console.log(`Example app is listening on port ${port}.`);
+// });
+
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+    connectionString: process.env.DB_URL,
 });
 
-app.listen(port, () => {
-console.log(`Example app is listening on port ${port}.`);
-});
+pool.connect()
+    .then(() => {
+        console.log('PostgreSQL connected successfully!');
+        return pool.end(); // Close connection after check
+    })
+    .catch((err) => {
+        console.error(' PostgreSQL connection failed:', err.message);
+    });
